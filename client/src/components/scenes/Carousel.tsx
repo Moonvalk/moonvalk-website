59276dd9a819ed0a654a6ef7b1709ce6bd36a152
Zack Harmon <elecgm@gmail.com>
2023-06-17 17:07:49 -0400

    Removed unnecessary packages/dependencies

--- client/src/components/scenes/Carousel.tsx
+++ client/src/components/scenes/Carousel.tsx
@@ -1,7 +1,6 @@
 import { ReactElement, useRef, useState } from "react";
 import { Mesh } from "three";
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
 import './Carousel.css';
 
 // export class Carousel extends React.Component {
@@ -105,14 +104,14 @@ function Box(props: any) {
 export default function Carousel(): ReactElement {
     return (
         <div className="carousel">
-          <Canvas>
+          {/* <Canvas>
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
             <pointLight position={[-10, -10, -10]} />
             <Box position={[-1.2, 0, 0]} />
             <Box position={[1.2, 0, 0]} />
             <OrbitControls />
-          </Canvas>
+          </Canvas> */}
         </div>
     );
 }
